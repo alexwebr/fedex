d9a1f3229a241fb212c6142690f7de5935502703
Alex Weber <alexwebr@gmail.com>
2015-06-09 21:35:37 -0500

    Do application startup properly; application:start(fedex) wasn't working.
    The rest of the app would start but the webserver wasn't started.

--- src/fedex_app.erl
+++ src/fedex_app.erl
@@ -8,24 +8,23 @@
 % Used when starting the app from debug mode like this:
 %   erl -pa ebin/ -boot start_sasl -s fedex
 start() ->
-  application:ensure_all_started(fedex),
-
-  Port = 8099,
-  inets:start(httpd, [
-    {port, Port},
-    {server_name, "localhost"},
-    {document_root, "webroot"},
-    {modules,[mod_esi, mod_get]},
-    {server_root, "."},
-    {erl_script_alias, {"/esi", [http_handler]}}
-  ]),
-  error_logger:info_report("Fedex started.").
+  application:ensure_all_started(fedex).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    error_logger:info_report("Fedex started."),
+    Port = 8099,
+    inets:start(httpd, [
+      {port, Port},
+      {server_name, "localhost"},
+      {document_root, "webroot"},
+      {modules,[mod_esi, mod_get]},
+      {server_root, "."},
+      {erl_script_alias, {"/esi", [http_handler]}}
+    ]),
     fedex_sup:start_link().
 
 stop(_State) ->
